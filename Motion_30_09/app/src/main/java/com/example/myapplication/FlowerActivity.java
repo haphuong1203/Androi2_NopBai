d0986fb2d1229be73ee070af6e2731cc094e841e
haphuong1203 <haphuongminyoongi@gmail.com>
2021-09-30 10:53:31 +0700

    commit

--- Motion_30_09/app/src/main/java/com/example/myapplication/FlowerActivity.java
+++ Motion_30_09/app/src/main/java/com/example/myapplication/FlowerActivity.java
@@ -1,14 +1,14 @@
-package com.example.motionlayout;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FlowerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flower);
     }
 }
